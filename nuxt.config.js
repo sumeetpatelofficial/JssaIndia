e9892de49ba10d9a161c66035b95c036006710c2
Sumeet Patel <sumeet@192.168.1.9>
2023-10-23 14:02:11 +0530

    updated nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/printCerti.scss"],
+  css: ["@/assets/printCerti.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["~/plugins/toast", "~/plugins/vue-inject.js"],
